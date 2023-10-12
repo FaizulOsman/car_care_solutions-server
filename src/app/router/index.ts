9873999e04a42d64c53811058aaf1089827b4df9
FaizulOsman <faizulosmanrabby@gmail.com>
2023-10-12 11:18:45 +0600

    feat: authentication functionality increment

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import { AuthRouter } from '../modules/auth/auth.router';
+import { UserRoutes } from '../modules/user/user.router';
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/',
-    route: '',
+    path: '/auth',
+    route: AuthRouter,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
   },
 ];
 
