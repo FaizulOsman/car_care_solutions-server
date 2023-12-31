ec9462c2de4163f8703de5516cb4457d30bb949b
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-31 20:54:35 +0600

    update: service functionality update

--- dist/app/modules/service/service.constants.js
+++ dist/app/modules/service/service.constants.js
@@ -1,5 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serviceSearchableFields = exports.serviceFilterableFields = void 0;
-exports.serviceFilterableFields = ['searchTerm', 'type', 'location'];
-exports.serviceSearchableFields = ['searchTerm', 'type', 'location'];
+exports.serviceFilterableFields = [
+    'searchTerm',
+    'status',
+    'type',
+    'location',
+];
+exports.serviceSearchableFields = [
+    'searchTerm',
+    'status',
+    'type',
+    'location',
+];
